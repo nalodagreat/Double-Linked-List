1cb08a6c7e9222aa20bdf43d024f15baf27abe58
nalodagreat <nalothegreat0@gmail.com>
2025-08-23 17:54:26 +0200

    feat:adding find node and insertNode after a Node

--- ConsoleApplication1/DblLinkedList.h
+++ ConsoleApplication1/DblLinkedList.h
@@ -46,6 +46,38 @@ public:
 		current->next = newNode;
 		newNode->prev = current;
 	}
-		
+	void insertAfter(Node* preevious, T value)
+	{
+		//head-1-2-3-null
+
+		if (head == NULL || preevious == NULL)
+		{
+			return;
+		}
+		Node* newNode = new Node();
+
+		newNode->value = value;
+		newNode->next = preevious->next;
+		newNode->prev = preevious;
+
+		if (preevious->next != NULL)
+		{
+			preevious->next->prev = newNode;
+		}
+		preevious->next = newNode;
+	}
+	Node * find(T value)
+	{
+		if (head == NULL)
+			return NULL;
+		Node* current = head;
+		while (current->next!=NULL)
+		{
+			if (current->value == value)
+				return current;
+			current = current->next;
+		}
+		return NULL;
+	}
 };
 
