208939b1045e62f21973fac1919d6d90e0af54cb
nalodagreat <nalothegreat0@gmail.com>
2025-08-25 23:18:46 +0200

    refcator:implementation of stack

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,6 +2,7 @@
 #include<string>
 #include <vector>
 #include "MyQueue.h"
+#include "Mystack.h"
 #include "DblLinkedList.h"
 using namespace std;
 int main()
@@ -90,6 +91,69 @@ int main()
 	cout << "After pop()" <<  endl;
 	queue.print();
 	
+	cout << "\n\n-----------------------------------------------------------------------------\n"
+		<< "\t\t\t\t\t\Stack\n";
+
+	cout << "-----------------------------------------------------------------------------\n\n";
+
+
+
+	MyStack <int> MyStack;
+
+	MyStack.push(10);
+	MyStack.push(20);
+	MyStack.push(30);
+	MyStack.push(40);
+	MyStack.push(50);
+
+
+	cout << "\nStack content: \n";
+	MyStack.print();
+
+	cout << "\nStack Size: " << MyStack.size();
+	cout << "\nStack Top: " << MyStack.Top();
+	cout << "\nStack Bottom: " << MyStack.Bottom();
+
+	MyStack.pop();
+
+	cout << "\n\nStack after pop() : \n";
+	MyStack.print();
+
+	//Extension #1
+	cout << "\n\n Item(2) : " << MyStack.getItem(2);
+
+	//Extension #2
+	MyStack.reverse();
+	cout << "\n\nStack after reverse() : \n";
+	MyStack.print();
+
+	//Extension #3
+	MyStack.updateItem(2, 600);
+	cout << "\n\nStack after updating Item(2) to 600 : \n";
+	MyStack.print();
+
+	//Extension #4
+	MyStack.insertAfter(2, 800);
+	cout << "\n\nStack after Inserting 800 after Item(2) : \n";
+	MyStack.print();
+
+
+	//Extension #5
+	MyStack.insertAtFront(1000);
+	cout << "\n\nStack after Inserting 1000 at top: \n";
+	MyStack.print();
+
+	//Extension #6
+	MyStack.insertAtBack(2000);
+	cout << "\n\nStack after Inserting 2000 at bottom: \n";
+	MyStack.print();
+
+	//Extension #7
+	MyStack.clear();
+	cout << "\n\nStack after Clear(): \n";
+	MyStack.print();
+
+
 
 	return 0;
 
